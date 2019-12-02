158dfe0d42cb6cfdad5f8e495b0badb8f96e5963
ebubekirbastama <ebubekirbastama@elmalicesmekuruyemis.com>
2019-12-02 21:15:27 +0200

    Add project files.

---
+++ Space_Clear/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Space_Clear
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxX1 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxX1
+            // 
+            // 
+            // 
+            // 
+            this.textBoxX1.Border.Class = "TextBoxBorder";
+            this.textBoxX1.Location = new System.Drawing.Point(142, 47);
+            this.textBoxX1.Name = "textBoxX1";
+            this.textBoxX1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Temizle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 172);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxX1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Space Clear";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.Controls.TextBoxX textBoxX1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Space_Clear/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+
+
+/// <summary>
+/// www.ebubekirbastama.com
+/// www.pintimarket.com
+/// www.uygunver.com
+/// whatshapp:905554128854
+/// 05554128854
+/// </summary>
+namespace Space_Clear
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxX1.Text= SapceClear(textBoxX1.Text);
+        }
+        ArrayList list = new ArrayList();
+        public string SapceClear(string veri)
+        {
+            for (int i = 0; i < veri.ToCharArray().Length; i++)
+            {
+                if (veri.ToCharArray()[i].ToString() != " ")
+                {
+                    list.Add(veri.ToCharArray()[i].ToString());
+                }
+            }
+            veri = "";
+            for (int i = 0; i < list.Count; i++)
+            {
+                veri += list[i].ToString();
+            }
+            list.Clear();
+            return veri;
+        }
+    }
+}
